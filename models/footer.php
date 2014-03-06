ef30e04ca8b40f079c288274756b18cb74878a72
jweijkamp <jweijkamp@hotmail.com>
2014-03-06 17:05:14 +0100

    Update footer.php

--- models/footer.php
+++ models/footer.php
@@ -7,7 +7,7 @@ echo"
 	<div id='line-seperator'>
 	<br />
 	<div id='footer'>
-	This website contains an experiment for the course Evolutionairy Algorithms of the Artificial Intelligence program at the <a href='http://www.ru.nl'>Radboud Universiteit Nijmegen</a> &copy; Janne Weijkamp en Mike Ligthart
+	This website contains an experiment for the course Evolutionairy Algorithms of the Artificial Intelligence program at the <a href='http://www.ru.nl/artificialintelligence'>Radboud Universiteit Nijmegen</a> &copy; Janne Weijkamp and Mike Ligthart
 	</div>
 	</div>
 </div>
